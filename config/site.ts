16404a30b5e7e4b47bed59829cdc31ed8a499467
Divyajyoti <divyajyoti1820@gmail.com>
2024-01-01 18:33:48 +0530

    TRELLO-CLONE : Project Corrupted

--- config/site.ts
+++
@@ -1,5 +0,0 @@
-export const siteConfig = {
-  name: "Taskify",
-  description:
-    "Make the impossible, possible with Trello. The ultimate teamwork project management tool. Start up a board in seconds, automate tedious tasks, ...",
-};
